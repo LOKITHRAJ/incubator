9e1f02ea959e39b69ec4c097b274f074fc73944c
lokithraj <lokithraj@gmail.com>
2023-01-20 17:01:07 +0530

    Init

---
+++ src/main/java/com/app/App.java
@@ -0,0 +1,45 @@
+package com.app;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) throws IOException {
+
+       int[]  a = {3,2,-2,7,5,7};
+
+       int max =  0;
+       int max2nd = 0;
+
+       for ( int i = 0 ; i < a.length ; i++ ) {
+
+           if (a[i] > max) {
+
+               max2nd = max;
+
+               max = a[i]; // 7
+
+               System.out.println(max2nd);
+               System.out.println(max);
+
+               if (max2nd < max ) {;
+                   max2nd = max;
+               }
+           }
+           else if(a[i] > max) {
+
+               max2nd = max;
+           }
+       }
+
+       System.out.println(max2nd);
+
+    }
+}
